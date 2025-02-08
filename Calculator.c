86d102f4f4a43a96f9527b272c953d774748163c
Kai-Wen-Xu <kai_xu@dlsu.edu.ph>
2025-02-08 15:11:42 +0800

    Add user input

--- Calculator.c
+++ Calculator.c
@@ -2,4 +2,13 @@
 
 int main()
 {
+	float fNumber1, fNumber2;
+	char opt;
+	printf("Enter 1st number:");
+	scanf("%f", &fNumber1);
+	printf("Enter 2nd number:");
+	scanf("%f", &fNumber2);
+	printf("Enter operator (+, -, *, /):");
+	scanf("%c", &opt);
+	
 }
