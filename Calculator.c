2512fca40b5db8bde0af40cb824cc04c882ac246
Your Namejaychou0116 <chieh-liang_chou@dlsu.edu.ph>
2025-02-08 15:14:39 +0800

    q

--- Calculator.c
+++ Calculator.c
@@ -2,4 +2,20 @@
 
 int main()
 {
+	float Ans;
+	if (opt == '/'){
+		Ans = fNumber1 / fNumber2;
+	}
+	
+	else if (opt == '*'){
+		Ans = fNumber1 * fNumber2;
+	}
+	
+	else if (opt == '+'){
+		Ans = fNumber1 + fNumber2;
+	}
+	
+	else if (opt == '-'){
+		Ans = fNumber1 - fNumber2;
+	}
 }
